b2b253704019fff486e821ec349641fd1ef99571
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-20 12:10:19 +0100

    created test styling

--- pages/News.jsx
+++ pages/News.jsx
@@ -1,7 +1,23 @@
+import { Button, Typography } from "@mui/material";
+
 const News = () => {
   return (
     <div>
-      <h1>News</h1>
+      <div style={{ display: "flex" }}>
+        <Typography variant="h2" component="h1">
+          NEW
+        </Typography>
+        <Typography component="h2" variant="h2" style={{ color: "#E2B945" }}>
+          S
+        </Typography>
+      </div>
+
+      <Button variant="contained" color="primary">
+        More news
+      </Button>
+      <Button variant="contained" color="secondary">
+        More news
+      </Button>
     </div>
   );
 };
