33006e60d5646aed084fea7b667602c487a71c80
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2022-12-21 14:59:09 +0000

    Prettified Code!

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,66 +1,78 @@
 import { Button, Typography, Box } from "@mui/material";
 import { useEffect } from "react";
 
-
-
 const Layout = () => {
-    // const videoRef = useRef()
+  // const videoRef = useRef()
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         videoRef.current.play()
-    //     }, 5000)
-    // }, [])
+  // useEffect(() => {
+  //     setTimeout(() => {
+  //         videoRef.current.play()
+  //     }, 5000)
+  // }, [])
 
   const style = {
-    width: '100%',
-    height: '100%',
-    position: 'relative', 
-    margin: 0
-  }
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    margin: 0,
+  };
 
   const textContent = {
-    width: {xs: '100%',sm: '25rem', md: '25rem', lg: '60rem'},
-    textAlign: 'center',
-    position: 'absolute',
-    top: '50%', 
-    left: { sm: '20%', md: '30%', lg: '15%', xl: '20%'},
-    
- 
-  }
+    width: { xs: "100%", sm: "25rem", md: "25rem", lg: "60rem" },
+    textAlign: "center",
+    position: "absolute",
+    top: "50%",
+    left: { sm: "20%", md: "30%", lg: "15%", xl: "20%" },
+  };
 
   const titleBox = {
     display: "flex",
-    flexDirection: {xs: 'column', sm: 'row'}
-  }
+    flexDirection: { xs: "column", sm: "row" },
+  };
 
   const title = {
-    fontSize: {xs: '2rem', lg: '5rem'}
-  }
+    fontSize: { xs: "2rem", lg: "5rem" },
+  };
 
   const lastTitle = {
-    marginLeft: {xs: "1.5rem", sm: '0.5rem', md: '0.5rem', lg: '1.5rem'},
-  }
-   
-    return (
-        <Box sx={style}>
-            <Box>
-                <video autoPlay muted loop style={{ width: '100%', height: '100%'}}>
-                 <source src="./rolling-web-edit.mp4" type="video/mp4" />
-                </video>
-            </Box>
-
-           <Box sx={textContent}>
-            <Box sx={titleBox}>
-                <Typography sx={title} variant="h1" component="h1">CLIMAX MUSIC</Typography>
-                <Typography sx={[title, lastTitle]} variant="h2" component="h2" style={{ color: "#E2B945"}}>EMPIRE</Typography>
-            </Box>
-                <Typography variant="p" component="p">Swedens aspiring music label</Typography>
-                <Button style={{marginTop: '1rem'}} variant="contained" color="primary">Contact us</Button>
-           </Box>
-         
-      </Box>
-    );
+    marginLeft: { xs: "1.5rem", sm: "0.5rem", md: "0.5rem", lg: "1.5rem" },
   };
-  
-  export default Layout;
+
+  return (
+    <Box sx={style}>
+      <Box>
+        <video autoPlay muted loop style={{ width: "100%", height: "100%" }}>
+          <source src="./rolling-web-edit.mp4" type="video/mp4" />
+        </video>
+      </Box>
+
+      <Box sx={textContent}>
+        <Box sx={titleBox}>
+          <Typography sx={title} variant="h1" component="h1">
+            CLIMAX MUSIC
+          </Typography>
+          <Typography
+            sx={[title, lastTitle]}
+            variant="h2"
+            component="h2"
+            style={{ color: "#E2B945" }}
+          >
+            EMPIRE
+          </Typography>
+        </Box>
+        <Typography variant="p" component="p">
+          Swedens aspiring music label
+        </Typography>
+        <Button
+          style={{ marginTop: "1rem" }}
+          variant="contained"
+          color="primary"
+        >
+          Contact us
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
