61a33191d4fa10aea5b0ca8baba6fe5fd5863dc4
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-21 15:50:14 +0100

    added burger menu and styling

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,65 +1,150 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import Image from "next/image";
 import logo from "../../assets/CLIMAXMUSICLOGOv3.png";
 import styles from "../../styles/Header.module.css";
 import Link from "next/link";
-import { style } from "@mui/system";
+import BurgerMenu from "../../assets/BurgerMenu.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
+  const burgerMenuBreakpoints = {
+    display: { xs: "block", sm: "block", md: "none", lg: "none", xl: "none" },
+    cursor: "pointer",
+  };
+
+  const navLinksBreakpoints = {
+    display: {
+      xs: "none",
+      sm: "none",
+      md: "block",
+      lg: "block",
+      xl: "block",
+    },
+  };
   return (
-    <header className={styles.header}>
-      <div className={styles.headerLeft}>
-        {/* <span id="burgermenu" class="material-icons burger-menu"> 
-          {" "}
-          menu{" "}
-        </span> */}
-        <Image
-          className={styles.logo}
-          src={logo}
-          alt="Climax Music Empire"
-          width={50}
-        />
-      </div>
-      <nav className={styles.headerRight}>
-        <ul className={styles.navLinks}>
-          <li id="home" className={styles.listItems}>
-            <Link className={styles.linkStyle} href="/News">
-              <Typography variant="p" component="p">
-                News
-              </Typography>
-            </Link>
-            {/* <a href="#"></a>Home */}
-          </li>
-          <li id="about-me" className={styles.listItems}>
-            <Link className={styles.linkStyle} href="/Gallery">
-              <Typography variant="p" component="p">
-                Gallery
-              </Typography>
-            </Link>
-          </li>
-          <li id="skills" className={styles.listItems}>
-            {/* REACT SCROLL LINK TO CONTACTS HERE */}
-            <Typography variant="p" component="p">
-              Contact
-            </Typography>
-          </li>
-          <li id="projects" className={styles.listItems}>
-            <Link className={styles.linkStyle} href="/Events">
-              <Typography variant="p" component="p">
-                Events
-              </Typography>
-            </Link>
-          </li>
-          <li id="contact" className={styles.listItems}>
-            <Link className={styles.linkStyle} href="/Artists">
-              <Typography variant="p" component="p">
-                Artists
-              </Typography>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <header className={isOpen ? [styles.headerOpen] : styles.header}>
+        <div className={styles.headerLeft}>
+          <Link href="/">
+            <Image
+              className={isOpen ? [styles.logoBurgerMenu] : styles.logo}
+              src={logo}
+              alt="Climax Music Empire"
+              width={50}
+            />
+          </Link>
+        </div>
+        {!isOpen && (
+          <nav className={styles.headerRight}>
+            <ul className={styles.navLinks}>
+              <li className={styles.listItems}>
+                <Link className={styles.linkStyle} href="/News">
+                  <Typography
+                    sx={navLinksBreakpoints}
+                    variant="p"
+                    component="p"
+                  >
+                    News
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItems}>
+                <Link className={styles.linkStyle} href="/Gallery">
+                  <Typography
+                    sx={navLinksBreakpoints}
+                    variant="p"
+                    component="p"
+                  >
+                    Gallery
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItems}>
+                {/* REACT SCROLL LINK TO CONTACTS HERE */}
+                <Typography sx={navLinksBreakpoints} variant="p" component="p">
+                  Contact
+                </Typography>
+              </li>
+              <li className={styles.listItems}>
+                <Link className={styles.linkStyle} href="/Events">
+                  <Typography
+                    sx={navLinksBreakpoints}
+                    variant="p"
+                    component="p"
+                  >
+                    Events
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItems}>
+                <Link className={styles.linkStyle} href="/Artists">
+                  <Typography
+                    sx={navLinksBreakpoints}
+                    variant="p"
+                    component="p"
+                  >
+                    Artists
+                  </Typography>
+                </Link>
+              </li>
+            </ul>
+            <Box onClick={toggleMenu} sx={burgerMenuBreakpoints}>
+              <Image src={BurgerMenu} alt="Menu" width={40} />
+            </Box>
+          </nav>
+        )}
+        {isOpen && (
+          <nav className={styles.headerRight}>
+            <Box onClick={toggleMenu} sx={burgerMenuBreakpoints}>
+              <Image src={BurgerMenu} alt="Menu" width={40} />
+            </Box>
+            <ul className={styles.navLinks}>
+              <li className={styles.listItemsOpen}>
+                <Link className={styles.linkStyle} href="/News">
+                  <Typography variant="p" component="p">
+                    News
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItemsOpen}>
+                <Link className={styles.linkStyle} href="/Gallery">
+                  <Typography variant="p" component="p">
+                    Gallery
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItemsOpen}>
+                {/* REACT SCROLL LINK TO CONTACTS HERE */}
+                <Typography variant="p" component="p">
+                  Contact
+                </Typography>
+              </li>
+              <li className={styles.listItemsOpen}>
+                <Link className={styles.linkStyle} href="/Events">
+                  <Typography variant="p" component="p">
+                    Events
+                  </Typography>
+                </Link>
+              </li>
+              <li className={styles.listItemsOpen}>
+                <Link className={styles.linkStyle} href="/Artists">
+                  <Typography variant="p" component="p">
+                    Artists
+                  </Typography>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
+    </div>
   );
 };
 
