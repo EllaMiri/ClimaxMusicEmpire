36d4e7b5463940cfe028faf6344bc0a6e475f79e
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-21 13:48:41 +0100

    added header to app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import CustomTheme from "../styles/muiTheme";
+import Header from "../components/layout/Header";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={CustomTheme}>
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );
