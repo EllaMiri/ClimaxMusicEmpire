7cf17fb2aa1dfe73aa28a99a53849dde73a0f15c
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-12-21 15:57:52 +0100

    styling for the video and text on both mobile and desktop

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Container } from "@mui/material";
+import { Button, Typography, Box } from "@mui/material";
 import { useEffect } from "react";
 
 
@@ -13,30 +13,53 @@ const Layout = () => {
     // }, [])
 
   const style = {
-    width: {xs: '300px', sm: '300px', md: '600px', lg: '1000px'}
+    width: '100%',
+    height: '100%',
+    position: 'relative', 
+    margin: 0
   }
 
-   
-   
+  const textContent = {
+    width: {xs: '100%',sm: '25rem', md: '25rem', lg: '60rem'},
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%', 
+    left: { sm: '20%', md: '30%', lg: '15%', xl: '20%'},
+    
+ 
+  }
+
+  const titleBox = {
+    display: "flex",
+    flexDirection: {xs: 'column', sm: 'row'}
+  }
+
+  const title = {
+    fontSize: {xs: '2rem', lg: '5rem'}
+  }
+
+  const lastTitle = {
+    marginLeft: {xs: "1.5rem", sm: '0.5rem', md: '0.5rem', lg: '1.5rem'},
+  }
    
     return (
-        <div style={{ position: 'relative', width: '100%', margin: 0 }}>
-            <div>
-                <video autoPlay muted loop>
+        <Box sx={style}>
+            <Box>
+                <video autoPlay muted loop style={{ width: '100%', height: '100%'}}>
                  <source src="./rolling-web-edit.mp4" type="video/mp4" />
                 </video>
-            </div>
+            </Box>
 
-           <div style={{ textAlign: 'center', position: 'absolute', top: '50%', left: '20%' }}>
-           <div style={{ display: "flex" }}>
-            <Typography variant="h1" component="h1">CLIMAX MUSIC</Typography>
-            <Typography variant="h2" component="h2" style={{fontSize: "6rem", marginLeft: "1.5rem", color: "#E2B945"}}>EMPIRE</Typography>
-         </div>
-            <Typography variant="p" component="p">Swedens aspiring music label</Typography>
-            <Button variant="contained" color="primary">Contact us</Button>
-           </div>
+           <Box sx={textContent}>
+            <Box sx={titleBox}>
+                <Typography sx={title} variant="h1" component="h1">CLIMAX MUSIC</Typography>
+                <Typography sx={[title, lastTitle]} variant="h2" component="h2" style={{ color: "#E2B945"}}>EMPIRE</Typography>
+            </Box>
+                <Typography variant="p" component="p">Swedens aspiring music label</Typography>
+                <Button style={{marginTop: '1rem'}} variant="contained" color="primary">Contact us</Button>
+           </Box>
          
-      </div>
+      </Box>
     );
   };
   
