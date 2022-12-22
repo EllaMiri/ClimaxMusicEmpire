0828fd9114b0bd87a13714ca844c9ee986527ac4
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-22 14:34:59 +0100

    created footer component

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,7 +1,61 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import logo from "../../assets/CLIMAXMUSICLOGOv3.png";
+import styles from "../../styles/Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer>
-      <div></div>
+      <div className={styles.upper}>
+        <Link
+          href="https://www.instagram.com/climaxmusicempire/"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            style={{ color: "white", fontSize: "2rem" }}
+            icon={faInstagram}
+          />
+        </Link>
+        <Link
+          href="https://www.facebook.com/profile.php?id=100085416694420"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faFacebook}
+            style={{
+              color: "white",
+              fontSize: "2rem",
+              paddingLeft: "5rem",
+              paddingRight: "5rem",
+            }}
+          />
+        </Link>
+        {/* INSERT LINK TO YOUTUBE HERE */}
+        <FontAwesomeIcon
+          icon={faYoutube}
+          style={{ color: "white", fontSize: "2rem" }}
+        />
+      </div>
+      <div className={styles.middle}>
+        <Image src={logo} alt="Climax Music Empire" width={50} />
+      </div>
+      <div className={styles.lower}>
+        <Link href="/News">
+          <span className={styles.links}>NEWS</span>
+        </Link>
+        <Link href="/Artists">
+          <span className={styles.links}>ARTISTS</span>
+        </Link>
+        {/* INSERT REACT SCROLL LINK HERE */}
+        <span className={styles.links}>CONTACT US</span>
+        <Link href="/Events">
+          <span className={styles.links}>EVENTS</span>
+        </Link>
+      </div>
     </footer>
   );
 };
