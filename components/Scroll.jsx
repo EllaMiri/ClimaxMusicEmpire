559d0a229be24d9d535f68b6f10573a38d8455ba
naku89 <anna.ozmehak@gmail.com>
2022-12-22 14:30:51 +0100

    added scroll icon from figma, and added a link arount it that takes you to the top of the page when pressed

---
+++ components/Scroll.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-scroll";
+import styles from '../styles/Scroll.module.css'
+
+const ScrollToTop = () => {
+
+    return (
+        <div className={styles.scrollContainer}>
+          <Link to="hero" spy={true} smooth={true} offset={50} duration={500}>
+            <img src="./scroll.png" alt="scrollIcon"/>
+          </Link>
+        </div>
+    )
+}
+
+export default ScrollToTop;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import ScrollToTop from "../components/Scroll";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/CLIMAXMUSICLOGOv3.png" />
       </Head>
       <Layout />
+     <ScrollToTop />
     </>
   );
 }
