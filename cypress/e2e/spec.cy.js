aedc34848a2af4e5c8990d309ca6cd3073efc29d
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-12-28 13:07:28 +0100

    changed the tests so it checks url for the right page, changed the cypress.config file and workflow files

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,62 +1,67 @@
 describe("Go To Climax Music Empire and Check Links In Header", () => {
   //goes to Climax Music Empire and clicks on the links in the header
   it("click the links in the header", () => {
-    cy.visit("http://localhost:3001/");
-
+    cy.visit("http://localhost:3000/");
     cy.contains("News").click();
+    cy.url().should('include', '/News')
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
+
+  it("Click Media in header", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("Media").click();
+    cy.url().should('include', '/Media')
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
 
-    cy.contains("Contact");
+  it("Click Contact in header", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Contact").click();
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
 
+  it("Click Events in header", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("Events").click();
-    cy.contains("Artists").click();
+    cy.url().should('include', '/Events')
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
 
+  it("Click Artists in header", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("Artists").click();
+    cy.url().should('include', '/Artists')
   });
 });
+
 
 describe("Check Links In Footer", () => {
   //goes to Climax Music Empire and clicks on the links in the footer
-  it("Clicks the links in the footer", () => {
-    cy.visit("http://localhost:3001/");
-
+  it("Clicks NEWS in footer", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("NEWS").click();
+    cy.url().should('include', '/News')
   });
 
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
-
-    cy.contains("ARTISTS").click();
+  it("Click Artists in footer", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Artists").click();
+    cy.url().should('include', '/Artists')
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
 
+  it("Clicks CONTACT US in footer", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("CONTACT US").click();
   });
-  it("Visits Climax Music Empire", () => {
-    cy.visit("http://localhost:3001/");
-
+  it("Clicks EVENTS in footer", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("EVENTS").click();
+    cy.url().should('include', '/Events')
   });
 });
 
+
 describe("Click Buttons On Page", () => {
   //goes to Climax Music Empire and click buttons to navigate
-  it("Clicks the links in the footer", () => {
-    cy.visit("http://localhost:3001/");
-
+  it("Clicks Out artists button on the page", () => {
+    cy.visit("http://localhost:3000/");
     cy.contains("Our artists").click();
+    cy.url().should('include', '/Artists')
   });
 });
