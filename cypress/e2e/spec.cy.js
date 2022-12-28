d0f27f21613e3d7ba136e0da8315eaf5c83dfc50
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2022-12-28 11:34:59 +0000

    Prettified Code!

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,62 +1,62 @@
-describe('Go To Climax Music Empire and Check Links In Header', () => {
+describe("Go To Climax Music Empire and Check Links In Header", () => {
   //goes to Climax Music Empire and clicks on the links in the header
-  it('click the links in the header', () => {
-    cy.visit('http://localhost:3001/')
+  it("click the links in the header", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('News').click()
-  })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
-    cy.contains('Media').click()
-  })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
+    cy.contains("News").click();
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
+    cy.contains("Media").click();
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('Contact')
-  })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
+    cy.contains("Contact");
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('Events').click()
-    cy.contains('Artists').click()
-  })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
+    cy.contains("Events").click();
+    cy.contains("Artists").click();
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('Artists').click()
-  })
-})
+    cy.contains("Artists").click();
+  });
+});
 
-describe('Check Links In Footer', () => {
+describe("Check Links In Footer", () => {
   //goes to Climax Music Empire and clicks on the links in the footer
-  it('Clicks the links in the footer', () => {
-    cy.visit('http://localhost:3001/')
+  it("Clicks the links in the footer", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('NEWS').click()
-  })
+    cy.contains("NEWS").click();
+  });
 
-    it('Visits Climax Music Empire', () => {
-      cy.visit('http://localhost:3001/')
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-      cy.contains('ARTISTS').click()
-    })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
+    cy.contains("ARTISTS").click();
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('CONTACT US').click()
-  })
-  it('Visits Climax Music Empire', () => {
-    cy.visit('http://localhost:3001/')
+    cy.contains("CONTACT US").click();
+  });
+  it("Visits Climax Music Empire", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('EVENTS').click()
-  })
-})
+    cy.contains("EVENTS").click();
+  });
+});
 
-describe('Click Buttons On Page', () => {
+describe("Click Buttons On Page", () => {
   //goes to Climax Music Empire and click buttons to navigate
-  it('Clicks the links in the footer', () => {
-    cy.visit('http://localhost:3001/')
+  it("Clicks the links in the footer", () => {
+    cy.visit("http://localhost:3001/");
 
-    cy.contains('Our artists').click()
-  })
-})
+    cy.contains("Our artists").click();
+  });
+});
