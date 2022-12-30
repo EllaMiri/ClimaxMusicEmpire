45e0251036e4eadc91db179957389e06c1e8ff20
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-12-30 10:22:17 +0100

    moved support false

--- cypressTest/cypress.config.js
+++ cypressTest/cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: "false",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: "false",
     baseUrl: "http://localhost:3000",
   },
 });
