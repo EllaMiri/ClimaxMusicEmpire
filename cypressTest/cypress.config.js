369733fcb553bdf44b02d90cdb990b32ed42c8fa
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-12-30 10:27:47 +0100

    removed "" on false in cypress config

--- cypressTest/cypress.config.js
+++ cypressTest/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: './cypress/support/e2e.js',
+    // supportFile: './cypress/support/e2e.js',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
