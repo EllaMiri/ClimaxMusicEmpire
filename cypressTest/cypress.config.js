1308fcd5cf4ab973b07d2ccd9482a53e24358ec5
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2022-12-30 09:26:18 +0000

    Prettified Code!

--- cypressTest/cypress.config.js
+++ cypressTest/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: './cypress/support/e2e.js',
+    supportFile: "./cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
