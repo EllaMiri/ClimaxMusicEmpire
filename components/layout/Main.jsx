63827ff5d849662604ec7914db4e461c739da975
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-02 18:09:36 +0100

    added news component to main component

--- components/layout/Main.jsx
+++ components/layout/Main.jsx
@@ -2,12 +2,14 @@ import Hero from "../Hero";
 import ScrollToTop from "../Scroll";
 import AboutUs from "../AboutUs";
 import Artist from "../Artist";
+import News from "../News";
 
 const Main = () => {
   return (
     <div>
       <Hero />
       <AboutUs />
+      <News />
       <Artist />
       <ScrollToTop />
     </div>
