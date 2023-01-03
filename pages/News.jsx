ec90aeb42461fbe2628c0268baa20bded517c5c4
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-03 13:42:59 +0100

    fixed css

--- pages/News.jsx
+++ pages/News.jsx
@@ -2,104 +2,210 @@ import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import styles from "../styles/AllNews.module.css";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const News = () => {
-  const style = {
-    color: { xs: "purple", sm: "red", md: "blue", lg: "pink", xl: "orange" },
-  };
-  return (
-    <Box
-    // className={styles.container}
-    >
-      <Box className={styles.titleContainer}>
-        <Typography variant="h2" component="h2">
-          NEW
-        </Typography>
-        <Typography className={styles.s} variant="h2" component="h2">
-          S
-        </Typography>
-      </Box>
-      <Box className={styles.container}>
-        <img
-          src="./shedondey.png"
-          alt="She Don Dey song by Icekid Ds"
-          className={styles.image}
-        />
-        <Box className={styles.textContainer}>
-          <Box className={styles.secondTitleBox}>
-            <Typography
-              variant="span"
-              component="span"
-              className={styles.secondTitle1}
-            >
-              Icekid Ds new song She Don Dey is
-            </Typography>
-            <Typography
-              variant="span"
-              component="span"
-              className={styles.secondTitle2}
-            >
-              out now!
-            </Typography>
-          </Box>
-          <Link
-            className={styles.link}
-            href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
-            target="_blank"
-          >
-            <Typography
-              variant="p"
-              component="p"
-              className={styles.clickToListen}
-            >
-              CLICK HERE TO LISTEN
-            </Typography>
-          </Link>
-          <Typography
-            className={styles.newsDescription}
-            component="p"
-            variant="p"
-          >
-            She Don Dey, made by Icekid Ds is his latest song, bringing afrobeat
-            and amapiano vibes with a beat produced by WavvyBeatz.
-          </Typography>
-        </Box>
-      </Box>
+  const [isMobile, setIsMobile] = useState(false);
 
-      <Box className={styles.reverseContainer}>
-        <img
-          src="./news-freshgee.png"
-          alt="She Don Dey song by Icekid Ds"
-          className={styles.image}
-        />
-        <Box className={styles.textContainer}>
-          <Box className={styles.secondTitleBox}>
-            <Typography
-              variant="span"
-              component="span"
-              className={styles.secondTitle1}
-            >
-              Freshgee signs with
+  useEffect(() => {
+    window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    const handleWindowResize = () => {
+      window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <Box>
+      {isMobile && (
+        <>
+          <Box className={styles.mobileTitleContainer}>
+            <Typography variant="h2" component="h2">
+              NEW
             </Typography>
-            <Typography
-              variant="span"
-              component="span"
-              className={styles.secondTitle2}
-            >
-              us!
+            <Typography className={styles.s} variant="h2" component="h2">
+              S
             </Typography>
           </Box>
-          <Typography
-            className={styles.newsDescription}
-            component="p"
-            variant="p"
-          >
-            We welcome Freshgee to our label! Freshgee is a young upcoming
-            afrobeat artist based in Nigeria. We are very happy to announce him
-            as part of our label.
-          </Typography>
-        </Box>
-      </Box>
+          <Box className={styles.mobileContainer}>
+            <Box className={styles.mobileTextContainer}>
+              <Box className={styles.secondTitleBox}>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle1}
+                >
+                  Icekid Ds new song She Don Dey is
+                </Typography>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle2}
+                >
+                  out now!
+                </Typography>
+              </Box>
+              <Link
+                className={styles.link}
+                href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
+                target="_blank"
+              >
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.clickToListen}
+                >
+                  CLICK HERE TO LISTEN
+                </Typography>
+              </Link>
+              <img
+                src="./shedondey.png"
+                alt="She Don Dey song by Icekid Ds"
+                className={styles.image}
+              />
+              <Typography
+                className={styles.mobileNewsDescription2}
+                component="p"
+                variant="p"
+              >
+                She Don Dey, made by Icekid Ds is his latest song, bringing
+                afrobeat and amapiano vibes with a beat produced by WavvyBeatz.
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box className={styles.mobileContainer}>
+            <Box className={styles.secondTitleBox}>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle1}
+              >
+                Freshgee signs with
+              </Typography>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle2}
+              >
+                us!
+              </Typography>
+            </Box>
+            <img
+              src="./news-freshgee.png"
+              alt="Nigerian afrobeat artist Freshgee"
+              className={styles.image}
+            />
+            <Box className={styles.mobileTextContainer}>
+              <Typography
+                className={styles.mobileNewsDescription}
+                component="p"
+                variant="p"
+              >
+                We welcome Freshgee to our label! Freshgee is a young upcoming
+                afrobeat artist based in Nigeria. We are very happy to announce
+                him as part of our label.
+              </Typography>
+            </Box>
+          </Box>
+        </>
+      )}
+      {!isMobile && (
+        <>
+          <Box className={styles.titleContainer}>
+            <Typography variant="h2" component="h2">
+              NEW
+            </Typography>
+            <Typography className={styles.s} variant="h2" component="h2">
+              S
+            </Typography>
+          </Box>
+          <Box className={styles.container}>
+            <img
+              src="./shedondey.png"
+              alt="She Don Dey song by Icekid Ds"
+              className={styles.image}
+            />
+            <Box className={styles.textContainer}>
+              <Box className={styles.secondTitleBox}>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle1}
+                >
+                  Icekid Ds new song She Don Dey is
+                </Typography>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle2}
+                >
+                  out now!
+                </Typography>
+              </Box>
+              <Link
+                className={styles.link}
+                href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
+                target="_blank"
+              >
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.clickToListen}
+                >
+                  CLICK HERE TO LISTEN
+                </Typography>
+              </Link>
+              <Typography
+                className={styles.newsDescription}
+                component="p"
+                variant="p"
+              >
+                She Don Dey, made by Icekid Ds is his latest song, bringing
+                afrobeat and amapiano vibes with a beat produced by WavvyBeatz.
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box className={styles.reverseContainer}>
+            <img
+              src="./news-freshgee.png"
+              alt="She Don Dey song by Icekid Ds"
+              className={styles.image}
+            />
+            <Box className={styles.textContainer}>
+              <Box className={styles.secondTitleBox}>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle1}
+                >
+                  Freshgee signs with
+                </Typography>
+                <Typography
+                  variant="span"
+                  component="span"
+                  className={styles.secondTitle2}
+                >
+                  us!
+                </Typography>
+              </Box>
+              <Typography
+                className={styles.newsDescription}
+                component="p"
+                variant="p"
+              >
+                We welcome Freshgee to our label! Freshgee is a young upcoming
+                afrobeat artist based in Nigeria. We are very happy to announce
+                him as part of our label.
+              </Typography>
+            </Box>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
