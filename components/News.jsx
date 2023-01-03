c457d11a13aa8100fa9e9d2fc5dc41daea8c294f
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-03 11:29:20 +0100

    made news on front page responsive

--- components/News.jsx
+++ components/News.jsx
@@ -3,109 +3,158 @@ import { Box } from "@mui/system";
 import Link from "next/link";
 import styles from "../styles/News.module.css";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const News = () => {
   const router = useRouter();
-  return (
-    <Box
-      className={styles.container}
-      //   style={{
-      //     display: "flex",
-      //     justifyContent: "space-around",
-      //     marginTop: "5rem",
-      //     marginLeft: "3rem",
-      //   }}
-    >
-      <img
-        src="./shedondey.png"
-        alt=""
-        className={styles.newsImage}
-        // style={{ width: "50%", height: "auto" }}
-      />
-      <Box
-        className={styles.textContainer}
-        // style={{
-        //   display: "flex",
-        //   flexDirection: "column",
-        //   alignItems: "center",
-        // }}
-      >
-        <Box className={styles.titleContainer}>
-          <Typography variant="h2" component="h2">
-            NEW
-          </Typography>
-          <Typography className={styles.s} variant="h2" component="h2">
-            S
-          </Typography>
-        </Box>
-        <Box className={styles.secondTitleBox}>
-          <Typography
-            variant="span"
-            component="span"
-            className={styles.secondTitle1}
-          >
-            Icekid Ds new song She Don Dey is
-          </Typography>
-          <Typography
-            variant="span"
-            component="span"
-            className={styles.secondTitle2}
-          >
-            out now!
-          </Typography>
-        </Box>
-        <Link
-          className={styles.link}
-          href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
-          target="_blank"
-        >
-          <Typography
-            variant="p"
-            component="p"
-            className={styles.clickToListen}
-          >
-            CLICK HERE TO LISTEN
-          </Typography>
-        </Link>
-        <Typography
-          className={styles.newsDescription}
-          component="p"
-          variant="p"
-        >
-          She Don Dey, made by Icekid Ds is his latest song, bringing afrobeat
-          and amapiano vibes with a beat produced by WavvyBeatz.
-        </Typography>
+  const [isMobile, setIsMobile] = useState(false);
 
-        {/* <Link
-          style={{ textDecoration: "none" }}
-          href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
-          target="_blank"
-        >
-          <Typography
-            variant="p"
-            component="p"
-            style={{
-              fontFamily: "Montserrat",
-              fontSize: "1rem",
-              marginBottom: "1rem",
-              marginTop: "1rem",
-            }}
-          >
-            CLICK HERE TO LISTEN
-          </Typography>
-        </Link> */}
-        <Box>
-          <Button
-            onClick={() => router.push("/News")}
-            className={styles.moreNewsBtn}
-            variant="contained"
-            color="secondary"
-          >
-            More news
-          </Button>
+  useEffect(() => {
+    window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    const handleWindowResize = () => {
+      window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <>
+      {isMobile && (
+        <Box className={styles.mobileContainer}>
+          <Box className={styles.mobileTextContainer}>
+            <Box className={styles.titleContainer}>
+              <Typography variant="h2" component="h2">
+                NEW
+              </Typography>
+              <Typography className={styles.s} variant="h2" component="h2">
+                S
+              </Typography>
+            </Box>
+            <Box className={styles.secondTitleBox}>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle1}
+              >
+                Icekid Ds new song She Don Dey is
+              </Typography>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle2}
+              >
+                out now!
+              </Typography>
+            </Box>
+            <Link
+              className={styles.link}
+              href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
+              target="_blank"
+            >
+              <Typography
+                variant="p"
+                component="p"
+                className={styles.clickToListen}
+              >
+                CLICK HERE TO LISTEN
+              </Typography>
+            </Link>
+            <img
+              src="./shedondey.png"
+              alt="She Don Dey song by Icekid Ds"
+              className={styles.newsImage}
+            />
+            <Typography
+              className={styles.mobileNewsDescription}
+              component="p"
+              variant="p"
+            >
+              She Don Dey, made by Icekid Ds is his latest song, bringing
+              afrobeat and amapiano vibes with a beat produced by WavvyBeatz.
+            </Typography>
+            <Box>
+              <Button
+                onClick={() => router.push("/News")}
+                className={styles.mobileMoreNewsBtn}
+                variant="contained"
+                color="secondary"
+              >
+                More news
+              </Button>
+            </Box>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      )}
+      {!isMobile && (
+        <Box className={styles.container}>
+          <img
+            src="./shedondey.png"
+            alt="She Don Dey song by Icekid Ds"
+            className={styles.newsImage}
+          />
+          <Box className={styles.textContainer}>
+            <Box className={styles.titleContainer}>
+              <Typography variant="h2" component="h2">
+                NEW
+              </Typography>
+              <Typography className={styles.s} variant="h2" component="h2">
+                S
+              </Typography>
+            </Box>
+            <Box className={styles.secondTitleBox}>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle1}
+              >
+                Icekid Ds new song She Don Dey is
+              </Typography>
+              <Typography
+                variant="span"
+                component="span"
+                className={styles.secondTitle2}
+              >
+                out now!
+              </Typography>
+            </Box>
+            <Link
+              className={styles.link}
+              href="https://distrokid.com/hyperfollow/icekidds1/she-don-dey"
+              target="_blank"
+            >
+              <Typography
+                variant="p"
+                component="p"
+                className={styles.clickToListen}
+              >
+                CLICK HERE TO LISTEN
+              </Typography>
+            </Link>
+            <Typography
+              className={styles.newsDescription}
+              component="p"
+              variant="p"
+            >
+              She Don Dey, made by Icekid Ds is his latest song, bringing
+              afrobeat and amapiano vibes with a beat produced by WavvyBeatz.
+            </Typography>
+            <Box>
+              <Button
+                onClick={() => router.push("/News")}
+                className={styles.moreNewsBtn}
+                variant="contained"
+                color="secondary"
+              >
+                More news
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
 
