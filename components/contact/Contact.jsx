a9a8a0227c6f17a58df666d017ce0a10d0880725
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2023-01-03 15:01:23 +0100

    merge conflict resolved

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import styles from "../styles/Contact.module.css";
+import styles from "../../styles/Contact.module.css";
 
 import ContactForm from "./ContactForm";
 
