999ae21eb25567c2062777f23f1d2bfb474bd215
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2023-01-04 14:27:39 +0100

    Co-authored-by: Ella Larsson <EllaMiri@users.noreply.github.com> mapping out the images in media, added styling for mobile and desktop

--- pages/Media.jsx
+++ pages/Media.jsx
@@ -3,42 +3,107 @@ import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
 import MediaSlideShow from "../components/GallerySlideShow";
+import { useState, useEffect } from "react";
+import styles from "../styles/Media.module.css"
 
 const Media = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    const handleWindowResize = () => {
+      window.innerWidth < 900 ? setIsMobile(true) : setIsMobile(false);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
-    <Box style={{paddingTop: '6rem', marginLeft: '2rem'}}>
-      <Box style={{display: 'flex'}}>
-        <Typography component="h2" variant="h2">
-          MEDI
-        </Typography>
-        <Typography
-          style={{color: '#e2b945'}}
-          component="h2"
-          variant="h2"
-        >
-          A
-        </Typography>
-      </Box>
-      <Box style={{display: 'flex', flexDirection: 'row-reverse'}}>
-        <Box style={{width: '50%', display: 'flex', justifyContent: 'center'}}>
-          <Image src='/galleryImage.png' alt="artist image" height={550} width={450} />
+    <>
+    {!isMobile &&
+    <Box style={{paddingTop: '6rem'}}>
+      <Box style={{display: 'flex'}}> 
+        
+          <Box style={{display: 'flex', marginBottom: '1rem', marginLeft: '3rem'}}>
+            <Typography component="h2" variant="h2">
+            MEDI
+            </Typography>
+            <Typography
+           style={{color: '#e2b945'}}
+           component="h2"
+            variant="h2"
+            >
+            A
+          </Typography>
+          </Box>
+        
         </Box>
-        <Box style={{width: '50%', display: 'flex', justifyContent: 'center', marginTop: '1rem'}}>
-          <Typography variant='p' component='p'>Lorem ipsum dolor sit amet,
+        <Box style={{ display: 'flex', justifyContent: 'center'}}>
+          <Box style={{width: '50%'}}>
+          <Typography style={{marginLeft: "3rem"}} variant='p' component='p'>Lorem ipsum dolor sit amet,
             consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
             nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.</Typography>
+          </Box>
+        <Box style={{width: '50%', display: 'flex', justifyContent: 'center'}}>
+        <img className={styles.image} src='/galleryImage.png' alt="artist image" />
         </Box>
       </Box>
+        
+      
       <Box style={{display: 'flex', justifyContent: 'center'}}>
         <FontAwesomeIcon icon={faAngleDown} style={{color: '#e2b945', fontSize: '10rem'}} />
       </Box>
       <MediaSlideShow />
     </Box>
+    }
+    {isMobile &&
+       <Box style={{paddingTop: '6rem',}}>
+        <Box style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}> 
+        
+          <Box style={{display: 'flex', marginBottom: '1rem'}}>
+            <Typography component="h2" variant="h2">
+            MEDI
+            </Typography>
+            <Typography
+           style={{color: '#e2b945'}}
+           component="h2"
+            variant="h2"
+            >
+            A
+           </Typography>
+            </Box>
+          <Box style={{width: "80%", display: 'flex', justifyContent: 'center'}}>
+          <Image layout="intrinsic" src='/galleryImage.png' alt="artist image" height={450} width={350} />
+        </Box>
+        </Box>
+        <Box style={{ display: 'flex', justifyContent: 'center'}}>
+          <Box style={{width: '90%', marginTop: '1rem'}}>
+          <Typography style={{textAlign: 'center'}} variant='p' component='p'>Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.</Typography>
+          </Box>
+      </Box>
+        
+      
+      <Box style={{display: 'flex', justifyContent: 'center'}}>
+        <FontAwesomeIcon icon={faAngleDown} style={{color: '#e2b945', fontSize: '10rem'}} />
+      </Box>
+      <MediaSlideShow />
+    </Box>
+    }
+   
 
+ </>
   );
 };
 
