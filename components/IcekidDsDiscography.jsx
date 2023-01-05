268e3ca7aad4a0382a96c5f615e9637f4087ff46
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-05 20:11:34 +0100

    using spotify embed

--- components/IcekidDsDiscography.jsx
+++ components/IcekidDsDiscography.jsx
@@ -1,4 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, getAutocompleteUtilityClass, Typography } from "@mui/material";
+import Spotify from "react-spotify-embed";
+import Iframe from "react-iframe";
 
 const IcekidDsDiscography = () => {
   return (
@@ -10,6 +12,32 @@ const IcekidDsDiscography = () => {
         <Typography style={{ color: "#e2b945" }} variant="h2" component="h2">
           Y
         </Typography>
+        <Box>
+          {/* <Spotify link="https://open.spotify.com/track/35tqPlW2KuQ77JtA1jDxIL?si=dccde9e6eaa84f2b"></Spotify> */}
+          <Box>
+            <iframe
+              src="https://open.spotify.com/embed/album/43LBs5VnAYmKjO4xyZts7w?utm_source=generator"
+              //   width="100%"
+              //   height="380"
+              frameBorder="0"
+              allowFullScreen=""
+              //   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              //   loading="lazy"
+            ></iframe>
+          </Box>
+          {/* <Box>
+            <iframe
+            
+              src="https://open.spotify.com/embed/album/5ke4vbdg2gV4PPXq8r7LzB?utm_source=generator"
+              width="100%"
+              height="380"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </Box> */}
+        </Box>
       </Box>
     </Box>
   );
