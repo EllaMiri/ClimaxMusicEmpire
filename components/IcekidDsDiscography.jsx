be268fac2f122071b68feb98e9a6251d75db98b5
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-05 19:12:18 +0100

    created discography component for icekid ds

---
+++ components/IcekidDsDiscography.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+const IcekidDsDiscography = () => {
+  return (
+    <Box style={{ display: "flex", justifyContent: "flex-end" }}>
+      <Box style={{ display: "flex" }}>
+        <Typography variant="h2" component="h2">
+          DISCOGRAPH
+        </Typography>
+        <Typography style={{ color: "#e2b945" }} variant="h2" component="h2">
+          Y
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default IcekidDsDiscography;
