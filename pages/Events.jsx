4636eea0273b9ffb6b980dbb27355610e3ad8db5
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2023-01-06 10:23:46 +0000

    Prettified Code!

--- pages/Events.jsx
+++ pages/Events.jsx
@@ -1,121 +1,188 @@
 import { Typography, Box } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
-import {faCalendar} from "@fortawesome/free-solid-svg-icons";
-import {faLocationDot} from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
-import styles from "../styles/Events.module.css"
+import styles from "../styles/Events.module.css";
 
 const events1MockData = [
   {
-    image: '/event.png',
-    name: 'Emergenza',
-    name1: 'Live',
-    location: 'Sticky finger',
-    time: '18:00',
-    date: '23 Sept 2022'
-  }
-]
+    image: "/event.png",
+    name: "Emergenza",
+    name1: "Live",
+    location: "Sticky finger",
+    time: "18:00",
+    date: "23 Sept 2022",
+  },
+];
 
 const events2MockData = [
   {
- image: '/event.png',
-    name: 'Emergenza',
-    name1: 'Live',
-    location: 'Sticky finger',
-    time: '18:00',
-    date: '23 Sept 2022'
-  }
-]
+    image: "/event.png",
+    name: "Emergenza",
+    name1: "Live",
+    location: "Sticky finger",
+    time: "18:00",
+    date: "23 Sept 2022",
+  },
+];
 
 const eventPageContainer = {
-  display: {xs: 'flex'},
-  flexDirection: {xs: 'column'},
-  alignItems: {xs: 'center', md: 'flex-start'},
-  marginLeft: {md: '2rem'}
-}
+  display: { xs: "flex" },
+  flexDirection: { xs: "column" },
+  alignItems: { xs: "center", md: "flex-start" },
+  marginLeft: { md: "2rem" },
+};
 
 const eventInformationContainer = {
-  flexDirection: {xs: 'column', md: 'row'},
-}
+  flexDirection: { xs: "column", md: "row" },
+};
 
 const secondEventInformationContainer = {
-  marginRight: {xs: '0', md: '4rem'},
-  flexDirection: {xs: 'column', md: 'row-reverse'},
-
-}
+  marginRight: { xs: "0", md: "4rem" },
+  flexDirection: { xs: "column", md: "row-reverse" },
+};
 
 const eventTitleBoxMediaQueries = {
-  marginBottom: {xs: '.5rem', md: '0'},
-  marginTop: {xs: '1rem', md: '0'}
-}
+  marginBottom: { xs: ".5rem", md: "0" },
+  marginTop: { xs: "1rem", md: "0" },
+};
 
 const informationMediaQueries = {
-  display: {xs: 'flex'},
-  flexDirection: {xs: 'column'},
-  alignItems: 'flex-start',
-  marginBottom: {xs: '1rem', md: '0'}
-}
+  display: { xs: "flex" },
+  flexDirection: { xs: "column" },
+  alignItems: "flex-start",
+  marginBottom: { xs: "1rem", md: "0" },
+};
 
 const Events = () => {
   return (
     <Box sx={eventPageContainer} className={styles.eventPageContainer}>
       <Box className={styles.titleBox}>
-        <Typography variant="h2" component="h2">EVENT</Typography>
-        <Typography variant="h2" component="h2" style={{ color: "#e2b945" }}>S</Typography>
+        <Typography variant="h2" component="h2">
+          EVENT
+        </Typography>
+        <Typography variant="h2" component="h2" style={{ color: "#e2b945" }}>
+          S
+        </Typography>
       </Box>
 
       <Box className={styles.eventsBoxes}>
-      {events1MockData.map((events) => (
-        <Box sx={eventInformationContainer} key={events.name} className={styles.eventInformationContainer}>
-          <Image src={events.image} alt="event image" height= {330} width= {300} />
-          <Box sx={informationMediaQueries}>
-            <Box sx={eventTitleBoxMediaQueries} style={{display: 'flex'}}>
-              <Typography variant="p" component="p" className={styles.eventTitle}>{events.name}</Typography>
-              <Typography variant="p" component="p" className={styles.eventTitleYellow}>{events.name1}</Typography>
-            </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faLocationDot} />
-              <Typography variant="p" component="p">{events.location}</Typography>
-            </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faClock} />
-              <Typography variant="p" component="p">{events.time}</Typography>
-            </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faCalendar} />
-              <Typography variant="p" component="p">{events.date}</Typography>
-            </Box>
-            </Box>
-        </Box>
-          )
-          )}
-        {events2MockData.map((events) => (
-          <Box sx={secondEventInformationContainer} key={events.name} style={{display: 'flex', alignItems: 'center'}}>
-            <Image src={events.image} alt="event image" height= {330} width= {300} style={{marginLeft: '1rem'}} />
+        {events1MockData.map((events) => (
+          <Box
+            sx={eventInformationContainer}
+            key={events.name}
+            className={styles.eventInformationContainer}
+          >
+            <Image
+              src={events.image}
+              alt="event image"
+              height={330}
+              width={300}
+            />
             <Box sx={informationMediaQueries}>
-              <Box sx={eventTitleBoxMediaQueries} style={{display: 'flex'}}>
-                <Typography variant="p" component="p" className={styles.eventTitle}>{events.name}</Typography>
-                <Typography variant="p" component="p" className={styles.eventTitleYellow}>{events.name1}</Typography>
+              <Box sx={eventTitleBoxMediaQueries} style={{ display: "flex" }}>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.eventTitle}
+                >
+                  {events.name}
+                </Typography>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.eventTitleYellow}
+                >
+                  {events.name1}
+                </Typography>
               </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faLocationDot} />
-              <Typography variant="p" component="p">{events.location}</Typography>
-            </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faClock} />
-              <Typography variant="p" component="p">{events.time}</Typography>
-            </Box>
-            <Box style={{display: 'flex'}}>
-              <FontAwesomeIcon className={styles.eventIcons} icon={faCalendar} />
-              <Typography variant="p" component="p">{events.date}</Typography>
-            </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  className={styles.eventIcons}
+                  icon={faLocationDot}
+                />
+                <Typography variant="p" component="p">
+                  {events.location}
+                </Typography>
+              </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon className={styles.eventIcons} icon={faClock} />
+                <Typography variant="p" component="p">
+                  {events.time}
+                </Typography>
+              </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  className={styles.eventIcons}
+                  icon={faCalendar}
+                />
+                <Typography variant="p" component="p">
+                  {events.date}
+                </Typography>
+              </Box>
             </Box>
           </Box>
-          )
-          )}
+        ))}
+        {events2MockData.map((events) => (
+          <Box
+            sx={secondEventInformationContainer}
+            key={events.name}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <Image
+              src={events.image}
+              alt="event image"
+              height={330}
+              width={300}
+              style={{ marginLeft: "1rem" }}
+            />
+            <Box sx={informationMediaQueries}>
+              <Box sx={eventTitleBoxMediaQueries} style={{ display: "flex" }}>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.eventTitle}
+                >
+                  {events.name}
+                </Typography>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className={styles.eventTitleYellow}
+                >
+                  {events.name1}
+                </Typography>
+              </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  className={styles.eventIcons}
+                  icon={faLocationDot}
+                />
+                <Typography variant="p" component="p">
+                  {events.location}
+                </Typography>
+              </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon className={styles.eventIcons} icon={faClock} />
+                <Typography variant="p" component="p">
+                  {events.time}
+                </Typography>
+              </Box>
+              <Box style={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  className={styles.eventIcons}
+                  icon={faCalendar}
+                />
+                <Typography variant="p" component="p">
+                  {events.date}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        ))}
       </Box>
-  
     </Box>
   );
 };
