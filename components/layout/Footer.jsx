dcd3d6aa832bbc2ff319a52f8ccfc5eb7563ca26
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-13 13:28:13 +0100

    added id for footer events

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -96,6 +96,7 @@ const Footer = () => {
         </Scroll>
         <Link style={{ textDecoration: "none" }} href="/Events">
           <Typography
+            id="footerEvents"
             variant="p"
             component="p"
             sx={lowerMediaQueries}
