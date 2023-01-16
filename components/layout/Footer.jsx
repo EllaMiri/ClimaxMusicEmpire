2ecf3320dc6c361085c95108abca52bef610baad
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-16 14:16:44 +0100

    contact scroll fix for footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,16 +3,35 @@ import styles from "../../styles/Footer.module.css";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import { Link as Scroll } from "react-scroll";
+import { Link as Scroll, scroller } from "react-scroll";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { useRouter } from "next/router";
 
 const Footer = () => {
   const lowerMediaQueries = {
     fontSize: { xs: ".7rem", sm: ".8rem", md: "1rem" },
   };
+
+  const router = useRouter();
+
+  const scrollTarget = (target) =>
+    scroller.scrollTo(target, {
+      smooth: true,
+      duration: 700,
+      offset: 670,
+      spy: true,
+    });
+
+  const scrollToPage = async (target) => {
+    if (router.pathname !== "/") {
+      await router.push("/");
+    }
+    scrollTarget(target);
+  };
+
   return (
     <footer>
       <Box className={styles.upper}>
@@ -85,16 +104,35 @@ const Footer = () => {
             ARTISTS
           </Typography>
         </Link>
-        <Scroll to="contact" spy={true} smooth={true} offset={0} duration={500}>
+        {router.pathname !== "/" && (
           <Typography
+            onClick={() => scrollToPage("contact")}
+            className={styles.contactLink}
+            sx={lowerMediaQueries}
             variant="p"
             component="p"
-            sx={lowerMediaQueries}
-            className={styles.contactLink}
           >
             CONTACT
           </Typography>
-        </Scroll>
+        )}
+        {router.pathname === "/" && (
+          <Scroll
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+          >
+            <Typography
+              className={styles.contactLink}
+              sx={lowerMediaQueries}
+              variant="p"
+              component="p"
+            >
+              CONTACT
+            </Typography>
+          </Scroll>
+        )}
         <Link style={{ textDecoration: "none" }} href="/Events">
           <Typography
             id="footerEvents"
