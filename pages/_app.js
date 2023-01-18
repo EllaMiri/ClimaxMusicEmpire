13ccf7f9424565ea1b21577e082a7859649b517f
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2023-01-18 21:07:38 +0100

    added cookie script for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,22 @@ import { ThemeProvider } from "@mui/material/styles";
 import CustomTheme from "../styles/muiTheme";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={CustomTheme}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </ThemeProvider>
+    <>
+      <Script
+        type="text/javascript"
+        charset="UTF-8"
+        src="//cdn.cookie-script.com/s/5a074dcc1fc1f55fdb1c4a4216f08caa.js"
+      ></Script>
+      <ThemeProvider theme={CustomTheme}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
