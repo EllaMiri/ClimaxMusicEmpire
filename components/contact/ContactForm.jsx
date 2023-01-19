74980fd57395daffbf0bfb8e39c069c5095573b1
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2023-01-19 15:18:35 +0000

    Prettified Code!

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -14,15 +14,14 @@ import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import styles from "../../styles/ContactForm.module.css";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
   const [showHide, setShowHide] = useState("showArtist");
   const [_, setShowRole] = useState("showArtist");
 
-  const submitNofify = () => toast.success("Email sent" )
-
+  const submitNofify = () => toast.success("Email sent");
 
   const form = useRef();
 
@@ -48,7 +47,7 @@ const ContactForm = () => {
           console.log(error.text);
         }
       );
-    submitNofify()
+    submitNofify();
   };
 
   const validationSchema = Yup.object().shape({
