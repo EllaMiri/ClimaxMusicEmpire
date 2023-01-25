410458b98f70323ea150e878a0136f12b8434edc
A-Ozmehak <A-Ozmehak@users.noreply.github.com>
2023-01-25 10:47:33 +0000

    Prettified Code!

--- pages/Artists/index.jsx
+++ pages/Artists/index.jsx
@@ -7,36 +7,36 @@ import { useRouter } from "next/router";
 
 const Artists = () => {
   const artistsBoxMediaQueries = {
-    paddingTop: { xs: "6rem" }
+    paddingTop: { xs: "6rem" },
   };
 
   const artistBoxMediaQueries = {
     display: { xs: "flex", lg: "flex" },
     flexDirection: { xs: "column", md: "row" },
     flexWrap: { md: "wrap" },
-    alignItems: { xs: "center" }
+    alignItems: { xs: "center" },
   };
 
   const boiOusMediaQueries = {
     marginTop: { xs: "1rem", lg: "5rem" },
     marginRight: { xs: "0", md: "1rem", lg: "1rem" },
-    marginBottom: { xs: "1rem", lg: "0" }
+    marginBottom: { xs: "1rem", lg: "0" },
   };
 
   const freshgeeMediaQueries = {
     marginTop: { xs: "1rem", lg: "7rem" },
     marginLeft: { xs: "0", md: "1rem", lg: "1rem" },
     marginRight: { xs: "0", md: "1rem", lg: "1rem" },
-    marginBottom: { xs: "1rem", lg: "0" }
+    marginBottom: { xs: "1rem", lg: "0" },
   };
 
   const titleBox = {
     justifyContent: { xs: "center", lg: "flex-start" },
-    marginLeft: { xs: "0", lg: "5rem" }
+    marginLeft: { xs: "0", lg: "5rem" },
   };
 
   const h1MediaQueries = {
-    fontSize: { xs: "2.5rem", sm: "4rem" }
+    fontSize: { xs: "2.5rem", sm: "4rem" },
   };
 
   const router = useRouter();
@@ -110,110 +110,109 @@ const Artists = () => {
             </Box>
           </Link>
         </Box>
-    
-      <Box>
-        <Box className={styles.artistNameBox}>
-          <Typography
-            style={{ fontFamily: "Montserrat" }}
-            component="p"
-            variant="p"
-          >
-            ICEKID
-          </Typography>
-          <Typography
-            component="p"
-            variant="p"
-            style={{ fontFamily: "Montserrat", marginLeft: "0.5rem" }}
-            className={styles.yellowText}
-          >
-            DS
-          </Typography>
-        </Box>
-
-        <Link href="/Artists/IcekidDs">
-          <Box className={styles.imageContainer}>
-            <img
-              src="./IcekidDsArtist.png"
-              alt="Artist IceKid DS"
-              className={styles.image}
-              style={{ height: "auto", width: "18rem" }}
-            />
-            <Box className={styles.overlay}>
-              <Typography className={styles.text}>READ MORE</Typography>
-            </Box>
+        <Box>
+          <Box className={styles.artistNameBox}>
+            <Typography
+              style={{ fontFamily: "Montserrat" }}
+              component="p"
+              variant="p"
+            >
+              ICEKID
+            </Typography>
+            <Typography
+              component="p"
+              variant="p"
+              style={{ fontFamily: "Montserrat", marginLeft: "0.5rem" }}
+              className={styles.yellowText}
+            >
+              DS
+            </Typography>
           </Box>
-        </Link>
-      </Box>
-  <Box sx={freshgeeMediaQueries}>
-    <Box className={styles.artistNameBox}>
-      <Typography
-        style={{ fontFamily: "Montserrat" }}
-        component="p"
-        variant="p"
-      >
-        FRESH
-      </Typography>
-      <Typography
-        component="p"
-        variant="p"
-        style={{ fontFamily: "Montserrat" }}
-        className={styles.yellowText}
-      >
-        GEE
-      </Typography>
-    </Box>
 
-    <Link href="/Artists/Freshgee">
-      <Box className={styles.imageContainer}>
-        <img
-          src="./FreshgeeArtist.png"
-          alt="Artist Freshgee"
-          className={styles.image}
-          style={{ height: "auto", width: "18rem" }}
-        />
-        <Box className={styles.overlay}>
-          <Typography className={styles.text}>READ MORE</Typography>
+          <Link href="/Artists/IcekidDs">
+            <Box className={styles.imageContainer}>
+              <img
+                src="./IcekidDsArtist.png"
+                alt="Artist IceKid DS"
+                className={styles.image}
+                style={{ height: "auto", width: "18rem" }}
+              />
+              <Box className={styles.overlay}>
+                <Typography className={styles.text}>READ MORE</Typography>
+              </Box>
+            </Box>
+          </Link>
         </Box>
-      </Box>
-    </Link>
-  </Box>
-  <Box>
-    <Box className={styles.artistNameBox}>
-      <Typography
-        style={{ fontFamily: "Montserrat" }}
-        component="p"
-        variant="p"
-      >
-        PRINCIPAL OF
-      </Typography>
-      <Typography
-        component="p"
-        variant="p"
-        style={{ fontFamily: "Montserrat", marginLeft: "0.5rem" }}
-        className={styles.yellowText}
-      >
-        MINDS
-      </Typography>
-    </Box>
+        <Box sx={freshgeeMediaQueries}>
+          <Box className={styles.artistNameBox}>
+            <Typography
+              style={{ fontFamily: "Montserrat" }}
+              component="p"
+              variant="p"
+            >
+              FRESH
+            </Typography>
+            <Typography
+              component="p"
+              variant="p"
+              style={{ fontFamily: "Montserrat" }}
+              className={styles.yellowText}
+            >
+              GEE
+            </Typography>
+          </Box>
 
-    <Link href="/Artists/PrincipalOfMinds">
-      <Box className={styles.imageContainer}>
-        <img
-          src="./PrincipalOfMindsArtist.png"
-          alt="Artist Principal of Minds"
-          className={styles.image}
-          style={{ height: "auto", width: "18rem" }}
-        />
-        <Box className={styles.overlay}>
-          <Typography className={styles.text}>READ MORE</Typography>
+          <Link href="/Artists/Freshgee">
+            <Box className={styles.imageContainer}>
+              <img
+                src="./FreshgeeArtist.png"
+                alt="Artist Freshgee"
+                className={styles.image}
+                style={{ height: "auto", width: "18rem" }}
+              />
+              <Box className={styles.overlay}>
+                <Typography className={styles.text}>READ MORE</Typography>
+              </Box>
+            </Box>
+          </Link>
         </Box>
+        <Box>
+          <Box className={styles.artistNameBox}>
+            <Typography
+              style={{ fontFamily: "Montserrat" }}
+              component="p"
+              variant="p"
+            >
+              PRINCIPAL OF
+            </Typography>
+            <Typography
+              component="p"
+              variant="p"
+              style={{ fontFamily: "Montserrat", marginLeft: "0.5rem" }}
+              className={styles.yellowText}
+            >
+              MINDS
+            </Typography>
+          </Box>
+
+          <Link href="/Artists/PrincipalOfMinds">
+            <Box className={styles.imageContainer}>
+              <img
+                src="./PrincipalOfMindsArtist.png"
+                alt="Artist Principal of Minds"
+                className={styles.image}
+                style={{ height: "auto", width: "18rem" }}
+              />
+              <Box className={styles.overlay}>
+                <Typography className={styles.text}>READ MORE</Typography>
+              </Box>
+            </Box>
+          </Link>
+        </Box>
+        ;
       </Box>
-    </Link>
-  </Box>;
-</Box>
-</Box>
-)
-  ;
+    </Box>
+  );
 };
 
 export default Artists;
